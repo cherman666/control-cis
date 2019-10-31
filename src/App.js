79a7fb5efc23e8b58679d258d6c6d5dfed512b05
cherman <santi.scheurmann@gmail.com>
2019-10-30 21:02:12 -0300

    Adding imports to App container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 function App() {
   return (
